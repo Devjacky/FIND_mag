5393ceddeff3f2f9c4f5b1a827877568e2f45601
Devjacky <parkjack2@gmail.com>
2016-02-12 06:00:58 +0900

    responsive nav test

--- js/find.js
+++ js/find.js
@@ -1,7 +1,7 @@
 (function ($) {
   $(document).ready(function(){
 
-    $(".header").hide();
+    $("#menu").hide();
 
     $(function () {
         $(window).scroll(function () {
@@ -13,6 +13,40 @@
             }
         });
     });
-
 });
-  }(jQuery));
+
+
+var menu = document.getElementById('menu'),
+    WINDOW_CHANGE_EVENT = ('onorientationchange' in window) ? 'orientationchange':'resize';
+
+$(function toggleHorizontal() {
+    [].forEach.call(
+        document.getElementById('menu').querySelectorAll('.custom-can-transform'),
+        function(el){
+            el.classList.toggle('pure-menu-horizontal');
+        }
+    );
+};
+
+function toggleMenu() {
+    // set timeout so that the panel has a chance to roll up
+    // before the menu switches states
+    if (menu.classList.contains('open')) {
+        setTimeout(toggleHorizontal, 500);
+    }
+    else {
+        toggleHorizontal();
+    }
+    menu.classList.toggle('open');
+    document.getElementById('toggle').classList.toggle('x');
+};
+
+function closeMenu() {
+    if (menu.classList.contains('open')) {
+        toggleMenu();
+    }
+}
+
+document.getElementById('toggle').addEventListener('click', function (e) {
+    toggleMenu();
+});
